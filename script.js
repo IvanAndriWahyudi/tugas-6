4dceba2af232e01ee2acb95f67f677f07dc6f57c
IvanAndriWahyudi <ivanandri.w04@gmail.com>
2022-01-09 19:52:36 +0700

    Switch case

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-console.log("If Else");
-var nilai = prompt("masukan nilai 1-10");
-console.log(nilai);
-if(nilai > 5){
-    console.log("Lulus");
-}else{
-    console.log("Tidak Lulus")
+
+var pilih = prompt("Masukan Pilihan =");
+switch(pilih){
+    case '1':
+        console.log("If Else");
+        var nilai = prompt("masukan nilai 1-10");
+        console.log(nilai);
+        if(nilai > 5){
+            console.log("Lulus");
+        }else{
+            console.log("Tidak Lulus")
+        }
+        break;
+    
 }
+
+
 
